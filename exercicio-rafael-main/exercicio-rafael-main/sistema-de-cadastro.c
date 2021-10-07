e6dbca2e7c3af9157a37fe2cb22de933d7695dc2
rrmeloo <92049854+rrmeloo@users.noreply.github.com>
2021-10-07 17:03:00 -0400

    projeto atualizado

--- exercicio-rafael-main/exercicio-rafael-main/sistema-de-cadastro.c
+++ exercicio-rafael-main/exercicio-rafael-main/sistema-de-cadastro.c
@@ -1,16 +1,16 @@
 /*
 CRIAR UM PROGRAMA QUE REALIZE O CADASTRO DE USUARIOS O PROGRAMA REALIZA AS SEGUINTES FUNCIONALIDADES:
 
-1 - REALIZAR O CADASTRO DO USUARIO, CADA USUARIO POSSUIRÁ NOME, LOGIN, SENHA, EMAIL E IDADE, APENAS O ADM COM LOGIN E SENHA
+1 - REALIZAR O CADASTRO DO USUARIO, CADA USUARIO POSSUIRÃ NOME, LOGIN, SENHA, EMAIL E IDADE, APENAS O ADM COM LOGIN E SENHA
 "ADM" E "12345" PODERA REALIZAR CADASTRO 
 
-2 - IMPLEMENTE UMA FUNÇÃO PARA REALIZAR A ATUALIZAÇÃO DE UM CADASTRO. ESSA FUNÇÃO SOMENTE PODERÁ SER FEITA PELO ADM OU PELO PROPRIO USER POR MEIO DE LOGIN E SENHA
+2 - IMPLEMENTE UMA FUNÃ‡ÃƒO PARA REALIZAR A ATUALIZAÃ‡ÃƒO DE UM CADASTRO. ESSA FUNÃ‡ÃƒO SOMENTE PODERÃ SER FEITA PELO ADM OU PELO PROPRIO USER POR MEIO DE LOGIN E SENHA
 
-3 - IMPLEMENTE UMA FUNÇÃO PARA IMPRIMIR TODOS OS USERS JUNTO COM TODAS AS SUAS INFOS.
+3 - IMPLEMENTE UMA FUNÃ‡ÃƒO PARA IMPRIMIR TODOS OS USERS JUNTO COM TODAS AS SUAS INFOS.
 
-4 - IMPLEMENTE UMA FUNÇÃO PARA DELETAR TODOS OS USUARIOS, APENAS O ADEMIRO PODERA REALIZAR ESSA AÇÃO
+4 - IMPLEMENTE UMA FUNÃ‡ÃƒO PARA DELETAR TODOS OS USUARIOS, APENAS O ADEMIRO PODERA REALIZAR ESSA AÃ‡ÃƒO
 
-OBS: IMPLEMENTE A SOLUÇÃO USANDO ALOCAÇÃO DINAMICA A CADA 5 USUARIOS CADASTRADOS
+OBS: IMPLEMENTE A SOLUÃ‡ÃƒO USANDO ALOCAÃ‡ÃƒO DINAMICA A CADA 5 USUARIOS CADASTRADOS
 */
 
 #include <stdio.h>
@@ -20,20 +20,23 @@ OBS: IMPLEMENTE A SOLUÇÃO USANDO ALOCAÇÃO DINAMICA A CADA 5 USUARIOS CADASTR
 main(){
   system ("color 02");
 
-  int escolha;
-
-  char login_adm[3];
-  char senha_adm[4];
+  int escolha, escolha2;
+  char login_adm[3], senha_adm[4];
 
   do{
-    printf ("\n [1] - LOGIN");
+	system("cls");
+
+    printf (" [1] - LOGIN");
     printf("\n [0] - FINALIZAR PROGRAMA");
     
     printf("\n Escolha: ");
     scanf("\n%d", &escolha);
 
+	//system("cls");
   switch (escolha){
-  case 1:
+	  system("title PORTAL DO ADM");
+	  
+  case 1://TELA DE LOGIN PARA O ADM
     printf("\n LOGIN: ");
     fflush(stdin);
     gets(login_adm);
@@ -43,14 +46,47 @@ main(){
     gets(senha_adm);
 
       if (strcmp(senha_adm, "12345") == 0 && strcmp(login_adm, "adm") == 0){
-        //MENU DE ADM
+        	
+		do{
+				system("cls");
+        	
+        		printf("\n BEM VINDO(A)! O QUE DESEJA FAZER?");
+        		printf("\n (1) - Realizar Cadastro de Usuário");
+        		printf("\n (2) - Atualizar Cadastro de Usuário");
+        		printf("\n (3) - Imprimir lista de usuários");
+        		printf("\n (4) - Deletar Base de Dados");
+        		printf("\n (0) - Sair");
+        		printf("\n Escolha: ");
+        		scanf("%d", &escolha2);
+        	
+        		switch (escolha2){
+        			case 1:
+        				realizar_cadastro();
+        				break;
+        		
+        			case 2:
+        				atualizar_dados();
+        				break;
+        			
+        			case 3:
+        				mostrar_usuarios();
+        				break;
+        			
+        			case 4:
+        				deletar_usuarios();
+        				break;
+        		
+        			default:
+        				printf("\nOpcao Invalida!");	
+			}
+		}		while(escolha2!=0);
       }else{
-      printf("\n Senha ou usuario incorreto!");
-      escolha = 9;
+      printf("\n Senha ou usuario incorreto!\n");
+      escolha = 9;	//forcar saida do do-while
       }
     break;
 
-  case 0:
+  case 0: // sair do sistema
     printf("\nAte mais!");
     break;
   
@@ -60,4 +96,46 @@ main(){
   }
   system("pause");
   } while (escolha !=0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void realizar_cadastro(){
+	//CADASTRO
+	printf("\n oi");
+}
+
+void atualizar_dados(){
+	//ATUALIZAR DADOS
+	printf("\n oi");	
+}
+
+void mostrar_usuarios(){
+	//MOSTRAR USERS	
+	printf("\n oi");
+}
+
+void deletar_usuarios(){
+	//DELETAR USUARIOS
+	printf("\n oi");	
 }
